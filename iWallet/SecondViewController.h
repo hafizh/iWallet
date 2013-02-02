f2cc4a4427c97bd02b54fc458c6a7b8201241d29
smorenoe <sebastianmorenoe@gmail.com>
2013-02-02 16:51:50 +0100

    1 - I created and changed the Tabbars Icons and the MainView(secondView). You can press on a category and see the texfield for enter an amount, the image, details of the category and the save & cancel button. If you press save or cancel, the user return to the main view.

--- iWallet/SecondViewController.h
+++ iWallet/SecondViewController.h
@@ -10,4 +10,43 @@
 
 @interface SecondViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIButton *b1;
+@property (weak, nonatomic) IBOutlet UIButton *b2;
+@property (weak, nonatomic) IBOutlet UIButton *b3;
+@property (weak, nonatomic) IBOutlet UIButton *b4;
+@property (weak, nonatomic) IBOutlet UIButton *b5;
+@property (weak, nonatomic) IBOutlet UIButton *b6;
+@property (weak, nonatomic) IBOutlet UIButton *b7;
+@property (weak, nonatomic) IBOutlet UIButton *b8;
+@property (weak, nonatomic) IBOutlet UIButton *b9;
+
+@property (weak, nonatomic) IBOutlet UITextField *textfield;
+@property (weak, nonatomic) IBOutlet UIButton *savebutton;
+@property (weak, nonatomic) IBOutlet UIButton *cancelbutton;
+
+@property (weak, nonatomic) IBOutlet UIImageView *category;
+
+@property (weak, nonatomic) IBOutlet UILabel *categoryLabel;
+
+- (IBAction)b1Action:(id)sender;
+- (IBAction)b2Action:(id)sender;
+- (IBAction)b3Action:(id)sender;
+- (IBAction)b4Action:(id)sender;
+- (IBAction)b5Action:(id)sender;
+- (IBAction)b6Action:(id)sender;
+- (IBAction)b7Action:(id)sender;
+- (IBAction)b8Action:(id)sender;
+- (IBAction)b9Action:(id)sender;
+
+- (IBAction)saveAction:(id)sender;
+- (IBAction)cancelAction:(id)sender;
+
+- (IBAction)textAction:(id)sender;
+
+
+
+
+
+
+
 @end
--- iWallet/SecondViewController.m
+++ iWallet/SecondViewController.m
@@ -26,4 +26,139 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)b1Action:(id)sender {
+    
+
+    [self changeViewFirst];
+    [_category setImage:[UIImage imageNamed:@"clothing.png"]];
+    [_categoryLabel setText:@"Clothing"];
+ 
+}
+
+- (IBAction)b2Action:(id)sender {
+    
+    [self changeViewFirst];
+    [_category setImage:[UIImage imageNamed:@"food.png"]];
+    [_categoryLabel setText:@"Food & Groceries"];
+    
+}
+
+- (IBAction)b3Action:(id)sender {
+    
+    [self changeViewFirst];
+    [_category setImage:[UIImage imageNamed:@"go.png"]];
+    [_categoryLabel setText:@"Going Out"];
+    
+}
+
+- (IBAction)b4Action:(id)sender {
+    
+    [self changeViewFirst];
+    [_category setImage:[UIImage imageNamed:@"health.png"]];
+    [_categoryLabel setText:@"Health Care & Cosmetics"];
+    
+}
+
+- (IBAction)b5Action:(id)sender {
+    
+    [self changeViewFirst];
+    [_category setImage:[UIImage imageNamed:@"household.png"]];
+    [_categoryLabel setText:@"Household & Rent"];
+    
+}
+
+- (IBAction)b6Action:(id)sender {
+    
+    [self changeViewFirst];
+    [_category setImage:[UIImage imageNamed:@"sport.png"]];
+    [_categoryLabel setText:@"Sport & Hobbies"];
+    
+}
+
+- (IBAction)b7Action:(id)sender {
+    
+    [self changeViewFirst];
+    [_category setImage:[UIImage imageNamed:@"study.png"]];
+    [_categoryLabel setText:@"Study Cost"];
+    
+}
+
+- (IBAction)b8Action:(id)sender {
+    
+    [self changeViewFirst];
+    [_category setImage:[UIImage imageNamed:@"transportation.png"]];
+    [_categoryLabel setText:@"Transportation & Travel"];
+    
+}
+
+- (IBAction)b9Action:(id)sender {
+    
+    [self changeViewFirst];
+    [_category setImage:[UIImage imageNamed:@"others.png"]];
+    [_categoryLabel setText:@"Others"];
+    
+}
+
+- (IBAction)saveAction:(id)sender {
+    
+    [self changeViewSecond];
+    //SAVE IN THE DATABASE
+
+    
+}
+
+- (IBAction)cancelAction:(id)sender {
+    
+    [self changeViewSecond];
+    
+}
+
+- (IBAction)textAction:(id)sender {
+    
+
+    
+}
+
+- (void) changeViewFirst
+{
+    
+    _b1.hidden = YES;
+    _b2.hidden = YES;
+    _b3.hidden = YES;
+    _b4.hidden = YES;
+    _b5.hidden = YES;
+    _b6.hidden = YES;
+    _b7.hidden = YES;
+    _b8.hidden = YES;
+    _b9.hidden = YES;
+    
+    _textfield.hidden = NO;
+    _savebutton.hidden = NO;
+    _cancelbutton.hidden = NO;
+    _category.hidden = NO;
+    _categoryLabel.hidden = NO;
+
+}
+
+- (void) changeViewSecond
+{
+    
+    _b1.hidden = NO;
+    _b2.hidden = NO;
+    _b3.hidden = NO;
+    _b4.hidden = NO;
+    _b5.hidden = NO;
+    _b6.hidden = NO;
+    _b7.hidden = NO;
+    _b8.hidden = NO;
+    _b9.hidden = NO;
+    
+    _textfield.hidden = YES;
+    _savebutton.hidden = YES;
+    _cancelbutton.hidden = YES;
+    _category.hidden = YES;
+    _categoryLabel.hidden = YES;
+    
+}
+
 @end
